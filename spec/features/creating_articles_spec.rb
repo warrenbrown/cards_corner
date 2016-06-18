424db421c47cbd5b5eef0d612817d92bd19d54d0
warrenbrown <warren@katapultmedia.com>
2016-06-18 17:29:01 -0500

    Articles can be deleted

--- spec/features/creating_articles_spec.rb
+++ spec/features/creating_articles_spec.rb
@@ -1,14 +1,21 @@
 require 'rails_helper'
 
 RSpec.feature 'users can create articles' do
-  scenario 'with valid attributes' do
+  before do
     visit '/'
     click_link 'Add Article'
-
+  end
+  scenario 'with valid attributes' do
     fill_in 'Title', with: 'My Title'
     click_button 'Create Article'
 
     expect(page).to have_content 'Article has been created.'
     expect(page).to have_content 'My Title'
+  end
+
+  scenario 'with invalid attributes' do
+    click_button 'Create Article'
+
+    expect(page).to have_content 'Article has not been created.'
   end
 end
--- spec/features/editing_articles_spec.rb
+++ spec/features/editing_articles_spec.rb
@@ -1,13 +1,15 @@
 require 'rails_helper'
 
 RSpec.feature 'Users can edit articles' do
-  scenario 'with valid attributes' do
+  before do
     article = FactoryGirl.create(:article, title: 'Sponge Bob')
 
     visit '/'
     click_link 'Sponge Bob'
     click_link 'Edit'
+  end
 
+  scenario 'with valid attributes' do
     fill_in 'Title', with: 'Patrick'
     click_button 'Update Article'
 
@@ -15,4 +17,10 @@ RSpec.feature 'Users can edit articles' do
     expect(page).to have_content 'Patrick'
   end
 
+  scenario 'with invalid attributes' do
+    fill_in 'Title', with: ' '
+    click_button 'Update Article'
+
+    expect(page).to have_content 'Article was not updated.'
+  end
 end
