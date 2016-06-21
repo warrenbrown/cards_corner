dc8ea04aa4e7cee4246c88420ee55e2565436728
warrenbrown <warren@katapultmedia.com>
2016-06-21 10:32:01 -0500

    added sidebar to show page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,7 +3,7 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( clean_blog/manifest.js clean_blog/manifest.css )
-
+Rails.application.config.assets.precompile += %w( 220logo.png)
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160620191210) do
+ActiveRecord::Schema.define(version: 20160621152743) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean  "published"
   end
 
   create_table "images", force: :cascade do |t|
