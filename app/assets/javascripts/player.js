04eef2935ecbdd93b5cb3db61fd4cfe82638a598
warrenbrown <warren@katapultmedia.com>
2016-07-12 10:54:14 -0500

    deleted screen divi

--- app/assets/javascripts/player.js
+++ app/assets/javascripts/player.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-window.addEventListener('load', function() {
+
 
   // Video container
   video = document.getElementById('video');
@@ -16,7 +16,6 @@ window.addEventListener('load', function() {
   soundButton = document.getElementsByClassName('fa fa-volume-up')[0];
   sbarContainer = document.getElementById('sbar-container');
   sbar = document.getElementById('sbar');
-  fullScreenButton = document.getElementsByClassName('fa fa-expand')[0];
 
   video.load();
   video.addEventListener('canplaythrough', function() {
@@ -26,27 +25,20 @@ window.addEventListener('load', function() {
     updatePlayer();
     soundButton.addEventListener('click', muteOrUnmute, false);
     sbarContainer.addEventListener('click', changeVolume, false);
-    fullScreenButton.addEventListener('click', fullScreen, false);
-    screenButton.addEventListener('click', playOrPause, false);
+
 
   }, false);
 
-}, false);
 });
 function playOrPause() {
   if (video.paused) {
       video.play();
       playButton.className = "fa fa-pause-circle";
       update = setInterval(updatePlayer, 30);
-
-      pauseScreen.style.display = 'none';
   } else {
       video.pause();
       playButton.className = "fa fa-play-circle";
       window.clearInterval(update);
-
-      pauseScreen.style.display = 'block';
-      screenButton.className = "fa fa-play-circle fa-4x";
   }
 }
 
@@ -57,9 +49,6 @@ function updatePlayer() {
   if (video.ended) {
     window.clearInterval(update);
     playButton.className = "fa fa-repeat";
-
-    pauseScreen.style.display = 'block';
-    screenButton.className = "fa fa-repeat fa-4x";
   }
 }
 
@@ -109,16 +98,4 @@ function changeVolume(ev) {
   video.muted = false;
   soundButton.className = "fa fa-volume-up";
   sbar.style.display = 'block';
-}
-
-function fullScreen() {
-  if (video.requestFullScreen) {
-    video.requestFullScreen();
-  } else if (video.webkitRequestFullScreen) {
-    video.webkitRequestFullScreen();
-  } else if (video.mozRequestFullScreen) {
-    video.mozRequestFullScreen();
-  } else if (video.msRequestFullScreen) {
-    video.msRequestFullScreen();
-  }
 }
